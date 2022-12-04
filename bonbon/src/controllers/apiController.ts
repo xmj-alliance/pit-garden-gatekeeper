657a88a4d1ad1cd7874f926573bad90888ce7665
Valorad the Oneiroseeker <valorad@outlook.com>
2022-12-04 18:55:08 +0000

    bonbon controller crud

--- bonbon/src/controllers/apiController.ts
+++ bonbon/src/controllers/apiController.ts
@@ -1,6 +1,14 @@
 import { AbstractController } from "./abstractController.ts";
 
+import { BonbonController } from "./bonbonController.ts";
+
 export class APIController extends AbstractController {
+  private bonbonController = new BonbonController();
+  constructor() {
+    super();
+    this.setupSubRouting();
+  }
+
   protected override setupRouting(): void {
     this.router.get("/", (ctx) => {
       ctx.response.status = 200;
@@ -9,5 +17,14 @@ export class APIController extends AbstractController {
         message: "API works!",
       };
     });
+  }
+
+  protected setupSubRouting(): void {
+    this.router
+      .use(
+        "/bonbons",
+        this.bonbonController.router.routes(),
+        this.bonbonController.router.allowedMethods(),
+      );
   }
 }
--- bonbon/src/controllers/bonbonController.ts
+++ bonbon/src/controllers/bonbonController.ts
@@ -1,13 +1,111 @@
+import { BonbonService } from "src/services/bonbon.service.ts";
+import { IBonbon, IInputBonbon } from "src/models/bonbon.interface.ts";
 import { AbstractController } from "./abstractController.ts";
 
 export class BonbonController extends AbstractController {
+  private bonbonService: BonbonService = new BonbonService();
+
   protected override setupRouting(): void {
-    this.router.get("/", (ctx) => {
+    // get all
+    this.router.get("/", async (ctx) => {
+      const searchParams = ctx.request.url.searchParams;
+
+      const searchToken = searchParams.get("q");
+
+      let bonbons: IBonbon[];
+
+      if (searchToken) {
+        bonbons = await this.bonbonService.search(searchToken);
+      } else {
+        bonbons = await this.bonbonService.getAll();
+      }
+
+      ctx.response.status = 200;
+      ctx.response.body = bonbons;
+    });
+
+    // add
+    this.router.post("/", async (ctx) => {
+      let ingressBonbons: any[] = await ctx.request.body().value;
+
+      if (!Array.isArray(ingressBonbons)) {
+        ctx.response.status = 400;
+        ctx.response.body = {
+          ok: false,
+          message: "Request body should be an array",
+          instances: null,
+        };
+        return;
+      }
+
+      const inputBonbons: IInputBonbon[] = this.inbound(ingressBonbons);
+
+      const addedBonBons = await this.bonbonService.add(inputBonbons);
+
       ctx.response.status = 200;
       ctx.response.body = {
         ok: true,
-        message: "Bonbon works!",
+        message: "Added",
+        instances: addedBonBons,
+      };
+    });
+
+    // update
+    this.router.patch("/", async (ctx) => {
+      const ingressBonbons: any[] = await ctx.request.body().value;
+
+      if (!Array.isArray(ingressBonbons)) {
+        ctx.response.status = 400;
+        ctx.response.body = {
+          ok: false,
+          message: "Request body should be an array",
+          instances: null,
+        };
+        return;
+      }
+
+      const inputBonbons: IInputBonbon[] = this.inbound(ingressBonbons);
+
+      const updatedBonBons = await this.bonbonService.update(inputBonbons);
+
+      ctx.response.status = 200;
+      ctx.response.body = {
+        ok: true,
+        message: "Updated.",
+        instances: updatedBonBons,
+      };
+    });
+
+    // delete
+    this.router.delete("/:id", async (ctx) => {
+      const deletingID = ctx.params.id;
+
+      if (!deletingID) {
+        ctx.response.status = 400;
+        ctx.response.body = {
+          ok: false,
+          message: "Deleting ID cannot be null",
+        };
+        return;
+      }
+
+      const deletedBonBons = await this.bonbonService.deleteByIDs([deletingID]);
+
+      ctx.response.status = 200;
+      ctx.response.body = {
+        ok: true,
+        message: "Deleted.",
+        instances: deletedBonBons,
       };
     });
   }
+
+  private inbound = (ingressBonbons: any[]) => {
+    return ingressBonbons.map((ele) => ({
+      id: ele.id,
+      dbname: ele.dbname,
+      popular: ele.popular,
+      count: ele.count,
+    } as IInputBonbon));
+  };
 }
